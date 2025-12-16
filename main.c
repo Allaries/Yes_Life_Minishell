fc8a19a03668f9ca6bc3e33c5be381db1c400398
Sarah Medeneche <meden.sarah@gmail.com>
2025-12-16 06:57:23 +0100

    trim les ' ' apres le decoupage |

--- main.c
+++ main.c
@@ -6,11 +6,37 @@
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 17:40:46 by smedenec          #+#    #+#             */
-/*   Updated: 2025/12/16 05:06:17 by smedenec         ###   ########.fr       */
+/*   Updated: 2025/12/16 06:56:24 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	**remove_space(char **str_split)
+{
+	int	i;
+	int	y;
+	int	len;
+	int	start;
+
+	i = 0;
+	y = 0;
+	len = 0;
+	start = 0;
+	while (str_split[i])
+	{
+		len = ft_strlen(str_split[i]);
+		if (len)
+		{
+			while (str_split[i][y] == ' ' || str_split[i][y] >= 8 && str_split[i][y] <= 13)
+				start++;
+			if (str_split[i][len - 1] == ' ')
+				str_split[i][len - 1] = '\0';
+		} // strtrim ??
+
+	}
+	return (str_split);
+}
 
 void	divise_pipe(char *str_stdint)
 {
@@ -22,6 +48,7 @@ void	divise_pipe(char *str_stdint)
 	str_split = ft_split(str_stdint, '|');
 	if (!str_split)
 		return ;
+	str_split = remove_space(str_split);
 	while (str_split[++i])
 		printf("%s\n", str_split[i]);
 }
@@ -73,7 +100,7 @@ int	main(void)
 // {
 // 	if (!str_stdint)
 // 		return ;
-// 	while ((str_stdint[i] >= 8 && str_stdint[i] <= 13) || str_stdint[i] == 32)
+// 	while ((str_stdint[i] >= 8 && str_stdint[i] <= 13) || str_stdint[i] == ' ')
 // 		i++;
 // 	if (str_stdint[i] == '"')
 // }

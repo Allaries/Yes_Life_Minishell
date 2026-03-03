a6fc238f9fbc64413529072ebd0b102ce25ff438
Remi <remi.richard@nect.fr>
2026-03-03 05:17:38 +0100

    premier tests signaux (tres tres sombre)

--- main.c
+++ main.c
@@ -12,15 +12,29 @@
 
 #include "minishell.h"
 
+// static void sigint_handler(int sig)
+// {
+//     (void)sig;
+//     write(1, "\n", 1);
+//     rl_on_new_line();
+//     rl_replace_line("", 0);
+//     rl_redisplay();
+// }
+
 int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
+	// struct	sigaction sa;
 	t_data	data;
 	int i = 2;
 
 	if (argc && argv) // Pour aucun warning
 		argc = 1;
 	data.envp = dupe_env(envp);
+    // sa.sa_handler = sigint_handler;
+    // sigemptyset(&sa.sa_mask);
+    // sa.sa_flags = 0;
+    // sigaction(SIGINT, &sa, NULL);
 	//SARAH
 	//JE VAIS ETRE CLAIR
 	//TU RAJOUTE UN -- A CE I, IL VA Y AVOIR DES BRICOLES
@@ -28,9 +42,10 @@ int	main(int argc, char **argv, char **envp)
 	while (i)
 	{
 		input = readline("mini-0.1$ ");
-		if (!input)
+		if (!input) //Ctrl-D
 		{
-			perror("Error");
+			free_tab(data.envp);
+			printf("exit\n");
 			return (1);
 		}
 		if (input && *input) 
--- minishell.h
+++ minishell.h
@@ -18,6 +18,7 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <signal.h>
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/stat.h>
